31a3d9b57ff613afeffa11c262554ff0bba43c7d
Ho Xin Zhen <hoxinzhen@gmail.com>
2024-02-22 15:30:05 +0800

    a

--- test.py
+++ test.py
@@ -23,8 +23,8 @@ results = processor.post_process_object_detection(outputs, target_sizes=target_s
 for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
     box = [round(i, 2) for i in box.tolist()]
     print(
-            "Detected {model.config.id2label[label.item()]} with confidence "
-            + "{round(score.item(), 3)} at location {box}"
+            f"Detected {model.config.id2label[label.item()]} with confidence "
+            + f"{round(score.item(), 3)} at location {box}"
     )
 
 # ## VIDEO CLASSIFICATION
